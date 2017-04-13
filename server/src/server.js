2beff51a82ecef566c547801edfd8f5b92b1b7ff
Peter Davey <pdavey@umass.edu>
2017-04-13 16:04:19 -0400

    static

--- server/src/server.js
+++ server/src/server.js
@@ -2,15 +2,7 @@
 var express = require('express');
 // Creates an Express server.
 var app = express();
-
-//import reverseString
-var util = require('./util');
-var reverseString = util.reverseString;
-
-// Defines what happens when it receives the `GET /` request
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static('../client/build'));
 
 // Starts the server on port 3000!
 app.listen(3000, function () {
@@ -19,16 +11,3 @@ app.listen(3000, function () {
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.text());
-
-// Handle POST /reverse [data]
-app.post('/reverse', function (req, res) {
-  // If the request came with text, then the text() middleware handled it
-  // and made `req.body` a string.
-  // Check that req.body is a string.
-  if (typeof(req.body) === 'string') {
-    var reversed = reverseString(req.body);
-    res.send(reversed);
-  } else {
-    res.status(400).end()
-  }
-});
